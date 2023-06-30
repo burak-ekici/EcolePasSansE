34900539249a961f3c51d28eabbdac7f905a330a
Ekici-Burak <b.ekici@it-students.fr>
2023-06-30 12:33:58 +0200

    mise en place du slider des des posts

---
+++ src/components/Caroussel.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-carousel
+    cycle
+    height="500"
+    hide-delimiter-background
+    show-arrows
+    interval="5000"
+  >
+    <v-carousel-item
+      v-for="post in props.posts"
+      :key="post.title"
+      cover
+      :src="post.src"
+      
+    >
+    <v-toolbar color="#001C30">
+      <v-toolbar-title class="text-center text-white text-button font-weight-medium">{{post.title}}</v-toolbar-title>
+    </v-toolbar>
+      <div class="carouselItemContent">
+        <h3></h3>
+        <p>{{post.description}}</p>
+        <v-btn color="#64CCC5" class="mt-6 text-white">En savoir plus</v-btn>
+      </div>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const props = defineProps(['posts'])
+
+
+</script>
+
+<style scoped>
+.carouselItemContent{
+  position:absolute;
+  top:60%;
+  left:50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+h3{
+  color:white;
+}
+p{
+  color:white;
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,13 +1,71 @@
 <template>
-  <div class="newsContainer pa-10">
-    <h2 class="text-center">Nouveautés</h2>
+  <div class="newsPageContainer pa-10">
+    <div class="subContainer">
+      <h2 class="text-center mb-8 block">Nouveautés</h2>
+      <Caroussel :posts="posts" />
+      <section class="posts">
+        <div v-for="post in posts" :key="post.title">
+          <h3>{{ post.title }}</h3>
+          <img :src="post.src" alt="">
+          <p>{{post.description}}</p>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import Caroussel from '@/components/Caroussel.vue'
 
+import { reactive } from 'vue';
+
+
+const posts = reactive([
+  {
+    src:"https://picsum.photos/1300/1080",
+    title: 'premier titre',
+    description : 'premiere description Just add your desired image size after our URL, and youll get a random image.'
+  },
+  {
+    src:"https://picsum.photos/1300/1080",
+    title: 'deuxieme titre',
+    description : 'deuxieme description Just add your desired image image.'
+  },
+  {
+    src:"https://picsum.photos/1300/1080",
+    title: 'troisieme titre',
+    description : 'troisieme description after our URL, and youll get a random image.'
+  },
+  {
+    src:"https://picsum.photos/1300/1080",
+    title: 'quatrieme titre',
+    description : 'quatrieme description Just add your desired image after our URL, and youll get a random image.'
+  }
+
+])
 </script>
 
 <style scoped>
+.newsPageContainer{
+  width:80vw;
+  margin:0 auto;
+  height:100%;
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  max-width:1000px;
+}
+.subContainer{
+  width:100%;
+}
 
+.posts h3{
+  display:block;
+  text-align: left;
+  margin-top:20px;
+}
+.posts img{
+  width:300px;
+  height:200px;
+}
 </style>
